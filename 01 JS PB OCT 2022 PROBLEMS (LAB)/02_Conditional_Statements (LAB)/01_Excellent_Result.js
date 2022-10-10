fc4febe77150b2077a71746226adc73e3ca0d3b8
Dimitar Sabev <97794162+HakunMatat@users.noreply.github.com>
2022-10-11 01:43:46 +0300

    commit

---
+++ 01 JS PB OCT 2022 PROBLEMS (LAB)/02_Conditional_Statements (LAB)/01_Excellent_Result.js
@@ -0,0 +1,10 @@
+function excellentResult(result) {
+  if (result[0] >= 5.5) {
+    console.log(`Excellent!`);
+  }
+}
+
+// excellentResult(["6"]);
+// excellentResult(["5"]);
+// excellentResult(["5.5"]);
+excellentResult(["5.49"]);
---
+++ 01 JS PB OCT 2022 PROBLEMS (LAB)/02_Conditional_Statements (LAB)/07_Area_Of_Figures.js
@@ -0,0 +1,20 @@
+function areasOfFigures(input) {
+  let figure = String(input[0]);
+  let side1 = Number(input[1]);
+  let side2 = Number(input[2]);
+
+  if (figure === "square") {
+    console.log(side1 * side1);
+  } else if (figure === "rectangle") {
+    console.log(side1 * side2);
+  } else if (figure === "circle") {
+    console.log(side1 ** 2 * Math.PI);
+  } else if (figure === "triangle") {
+    console.log((side1 * side2) / 2);
+  }
+}
+
+// areasOfFigures(["square", "5"]);
+// areasOfFigures(["rectangle", "7", "2.5"]);
+// areasOfFigures(["circle", "6"]);
+// areasOfFigures(["triangle", "4.5", "20"]);
