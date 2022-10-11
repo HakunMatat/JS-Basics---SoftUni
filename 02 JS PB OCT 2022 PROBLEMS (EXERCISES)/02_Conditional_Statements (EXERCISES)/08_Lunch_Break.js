d22fab5237b2c83408d12f64da4c19d089cdf3a3
Dimitar Sabev <97794162+HakunMatat@users.noreply.github.com>
2022-10-11 09:43:02 +0300

    commit

---
+++ 02 JS PB OCT 2022 PROBLEMS (EXERCISES)/02_Conditional_Statements (EXERCISES)/08_Lunch_Break.js
@@ -0,0 +1,17 @@
+function lunchBreak(index) {
+    let movieName = index[0];
+    let movieLenght = Number(index[1]);
+    let movieBreak = Number(index[2]);
+
+    let timeLeft = movieBreak - movieBreak * (1 / 8) - movieBreak * (1 / 4);
+    console.log(timeLeft);
+
+    if (movieLenght <= timeLeft) {
+        console.log(`You have enough time to watch ${movieName} and left with ${Math.ceil(timeLeft - movieLenght)} minutes free time.`);
+    } else {
+        console.log(`You don't have enough time to watch ${movieName}, you need ${Math.ceil(movieLenght - timeLeft)} more minutes.`);
+    }
+}
+
+lunchBreak(['Game of Thrones', '60', '96']);
+// lunchBreak(['Teen Wolf', '48', '60']);
