4d2a982ad387b25eed389879786f813d3e4e08fb
Dimitar Sabev <97794162+HakunMatat@users.noreply.github.com>
2022-10-26 21:13:44 +0300

    commit

--- 01 JS PB OCT 2022 PROBLEMS (LAB)/04_For_Loop (LAB)/04_Even_Powers_Of_2.js
+++ 01 JS PB OCT 2022 PROBLEMS (LAB)/04_For_Loop (LAB)/04_Even_Powers_Of_2.js
@@ -1,7 +1,7 @@
 function evenPowersOf2(input) {
     let n = Number(input[0]);
-    for (i = 1; ; i = Math.pow(i, n)) {
-        console.log(i);
+    for (let i = 0; i <= n; i += 2) {
+        console.log(Math.pow(2, i));
     }
 }
-evenPowersOf2(['3']);
+evenPowersOf2(['7']);
