5c3121488fcc16a0ff450da3286381d0310852ec
Dimitar Sabev <97794162+HakunMatat@users.noreply.github.com>
2022-11-19 13:24:51 +0200

    new exercise

--- 02 JS PB OCT 2022 PROBLEMS (EXERCISES)/04_While_Loop (EXERCISES)/01_Old_Books.js
+++ 02 JS PB OCT 2022 PROBLEMS (EXERCISES)/04_While_Loop (EXERCISES)/01_Old_Books.js
@@ -14,7 +14,7 @@ function oldBooks(input) {
     }
 
     if (bookIsFound === false) {
-        console.log(`The book you seach is not here!`);
+        console.log(`The book you search is not here!`);
         console.log(`You checked ${index - 1} books.`);
     } else {
         console.log(`You checked ${index - 1} books and found it.`);
